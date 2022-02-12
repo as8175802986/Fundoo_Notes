3a2ca843ea8b8306f0345931f027ca8cd2ea1707
“Arunesh <as8175802986@gmail.com>
2022-02-12 05:44:13 +0530

    [Arunesh] added code for Validatiom

--- FundooNotes/RepositoryLayer/Services/UserRL.cs
+++ FundooNotes/RepositoryLayer/Services/UserRL.cs
@@ -188,42 +188,7 @@ namespace RepositoryLayer.Class
                 throw e;
             }
         }
-        public string EncryptPassword(string password)
-        {
-            try
-            {
-                byte[] encode = new byte[password.Length];
-
-                encode = Encoding.UTF8.GetBytes(password);
-                //byte encPassword = Convert.FromBase64String(encode);
-                string encPassword = Convert.ToBase64String(encode);
-                return encPassword;
-            }
-            catch (Exception)
-            {
-                throw;
-            }
-        }
-        public string DecryptPassword(string encryptpwd)
-        {
-            try
-            {
-                UTF8Encoding encodepwd = new UTF8Encoding();
-                Decoder Decode = encodepwd.GetDecoder();
-                byte[] todecode_byte = Convert.FromBase64String(encryptpwd);
-                int charCount = Decode.GetCharCount(todecode_byte, 0, todecode_byte.Length);
-                char[] decoded_char = new char[charCount];
-                Decode.GetChars(todecode_byte, 0, todecode_byte.Length, decoded_char, 0);
-                string decryptpwd = new String(decoded_char);
-                return decryptpwd;
-            }
-            catch (Exception)
-            {
-
-                throw;
-            }
-        }
-
+        
         private void msmqQueue_ReceiveCompleted(object sender, ReceiveCompletedEventArgs e)
         {
             try
